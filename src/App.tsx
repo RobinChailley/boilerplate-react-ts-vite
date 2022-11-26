d2359931f7ef7e65cf946a15d63cac1dcdc87348
Robin Chailley <robin.chailley@origame.fr>
2022-11-26 17:59:56 +0100

    feat: fix eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-export function App() {
-  return (
+const App = () => {
+    return (
         <div className='App'>
             <header className='App-header'>
                 <p>Hello Vite + React!</p>
             </header>
         </div>
-  )
+    )
 }
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from '@/app'
+import App from '@/App'
 import './index.css'
 
+// eslint-disable-next-line max-len
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <App/>
