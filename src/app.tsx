eb32c30502f6099145c086bc93ce83fd6fcc9a6c
Robin Chailley <robin.chailley@origame.fr>
2022-11-26 17:36:02 +0100

    init commit

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+export function App () {
+  return (
+        <div className='App'>
+            <header className='App-header'>
+                <p>Hello Vite + React!</p>
+            </header>
+        </div>
+  )
+}
